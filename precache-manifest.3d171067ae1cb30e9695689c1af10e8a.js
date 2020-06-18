55dfad1c35d95089c0e3802317a0057316bdd8c3
Đức Anh Tài <39086667+datai999@users.noreply.github.com>
2020-06-18 18:00:19 +0700

    Updates

---
+++ precache-manifest.3d171067ae1cb30e9695689c1af10e8a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83d72418b9aa4db48d23eb34f4e92827",
+    "url": "/cv/index.html"
+  },
+  {
+    "revision": "50d815fa5e2a3bddd0b5",
+    "url": "/cv/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c4baae96fe1a2a48f37a",
+    "url": "/cv/static/js/2.d8391d04.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cv/static/js/2.d8391d04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50d815fa5e2a3bddd0b5",
+    "url": "/cv/static/js/main.bdb7d99d.chunk.js"
+  },
+  {
+    "revision": "dd7564ccb48bd4563785",
+    "url": "/cv/static/js/runtime-main.3fa2ccda.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cv/static/media/logo.5d5d9eef.svg"
+  }
+]);
